9c1d339d53fffc77bf0ebeaab56b336da77f6309
Michael Behrens <michael.behrens@hadiko.de>
2024-01-31 17:42:11 +0100

    Support named buttons

--- midicontroller/xtouch.js
+++ midicontroller/xtouch.js
@@ -1,22 +1,22 @@
 import { MidiController } from "../midiabstract/midi.js";
 import { MidiButtonEntity, MidiBinaryLamp, MidiFaderEntity, MidiFaderMotorEntity, MidiEncoderEntity, MidiDimmerLampEntity } from "../midiabstract/midi_entities.js";
 
-// var button_names = new BiMap({
-//     "buttonEncoderDec15": 46,
-//     "buttonEncoderInc15": 47,
-//     "buttonEncoderDec16": 48,
-//     "buttonEncoderInc16": 49,
-//     "buttonMain": 50,
-//     "buttonA": 84,
-//     "buttonB": 85,
-//     "buttonLoop": 86,
-//     "buttonPrev": 91,
-//     "buttonNext": 92,
-//     "buttonStop": 93,
-//     "buttonPlay": 94,
-//     "buttonRec": 95,
-//     "buttonTouchMain": 112,
-// })
+const named_buttons = [
+    // "buttonEncoderDec15": 46,
+    // "buttonEncoderInc15": 47,
+    // "buttonEncoderDec16": 48,
+    // "buttonEncoderInc16": 49,
+    {name: "buttonMain", id: 50},
+    {name: "buttonA",id: 84},
+    {name: "buttonB",id: 85},
+    {name: "buttonLoop",id: 86},
+    {name: "buttonPrev",id: 91},
+    {name: "buttonNext",id: 92},
+    {name: "buttonStop",id: 93},
+    {name: "buttonPlay",id: 94},
+    {name: "buttonRec",id: 95},
+    // "buttonTouchMain": 112,
+]
 
 export class XTouchCompactMidiController extends MidiController {
     constructor() {
@@ -35,8 +35,17 @@ export class XTouchCompactMidiController extends MidiController {
             this.buttons[2].push(new XTouchButton("mil_" + (i + 1), this, 24 + i));
             this.buttons[3].push(new XTouchButton("bot_" + (i + 1), this, i));
         }
-        this.buttons[3].push(new XTouchButton("main", this, 50));
-        this.faders.push(new XTouchFader("main", this, 8));
+
+        this.textButtons = {}
+        named_buttons.map((item) => {
+            this.textButtons[item.name] = new XTouchButton(item.name, this, item.id);
+        });
+
+        this.buttons[3].push(this.textButtons["buttonMain"]);
+
+        var main_fader = new XTouchFader("main", this, 8);
+        main_fader.touch = new MidiButtonEntity("touchmain", this, 112)
+        this.faders.push(main_fader);
 
         this.encoders = [];
         for (let i = 0; i < 14; i++) {
@@ -56,7 +65,7 @@ class XTouchFader {
         // TODO all IDs
         this.fader = new MidiFaderEntity(name, midi_device, id);
         this.motor = new MidiFaderMotorEntity(name, midi_device, id);
-        this.touch = new MidiButtonEntity(name, midi_device, 104 + id);
+        this.touch = new MidiButtonEntity("touch" + name, midi_device, 104 + id);
     }
 
     set(...args) {
